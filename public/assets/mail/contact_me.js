1dade2da22f9a5ba0c78a9e2b51bf98808a5ecf0
Alex Lanthier <lanthier72@gmail.com>
2022-01-23 12:26:47 -0600

    updates

---
+++ public/assets/mail/contact_me.js
@@ -0,0 +1,87 @@
+$(function () {
+    $(
+        "#contactForm input,#contactForm textarea,#contactForm button"
+    ).jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault();
+
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+            var firstName = name; 
+
+            if (firstName.indexOf(" ") >= 0) {
+                firstName = name.split(" ").slice(0, -1).join(" ");
+            }
+            $this = $("#send-message-button");
+            $this.prop("disabled", true);
+            $.ajax({
+                url: "https://us-central1-silent-venture-312421.cloudfunctions.net/contact-email",
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    message: message,
+                },
+                dataType: 'json',
+                cache: false,
+                success: function () {
+                    // Success message
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong>Your message has been sent. </strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $("#success").html("<div class='alert alert-danger'>");
+                    $("#success > .alert-danger")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-danger").append(
+                        $("<strong>").text(
+                            "Sorry " +
+                                firstName +
+                                ", it seems that my mail server is not responding. Please email lanthier@lanthierlabs.com!"
+                        )
+                    );
+                    $("#success > .alert-danger").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                },
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
---
+++ public/js/scripts.js
@@ -0,0 +1,87 @@
+/*!
+* Start Bootstrap - Freelancer v6.0.6 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using anime.js
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                anime({
+                    targets: 'html, body',
+                    scrollTop: target.offset().top - 72,
+                    duration: 1000,
+                    easing: 'easeInOutExpo'
+                });
+                return false;
+            }
+        }
+    });
+
+    // Scroll to top button appear
+    $(document).scroll(function () {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+    });
+
+    $('#websites-button').click(function () {
+        setTimeout(() => {
+            const dropdown = document.getElementById('contact-type');
+            dropdown.focus();
+            dropdown.value = 'Website';
+        }, 1000);
+    });
+
+    $('#teaching-button').click(function () {
+        setTimeout(() => {
+            const dropdown = document.getElementById('contact-type');
+            dropdown.focus();
+            dropdown.value = 'Teaching';
+        }, 1000);
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 350) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
+
+function setDropdown(dropdownValue) {
+    const dropdown = document.getElementById('contact-type');
+    dropdown.value = dropdownValue;
+}
